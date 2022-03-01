c3ba675d941ef525b5aa99e94178700043f8ebad
three-cubed <72698356+three-cubed@users.noreply.github.com>
2022-03-01 09:13:28 +0000

    slight changes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 app.use(express.static(__dirname)); // To access stylesheet.css
 app.set('view engine', 'ejs');
 
+// If you want to use app.use(express.json()); then put it before the router!
 const router = require('./routes/routes');
-
 app.use('/', router);
 
-app.listen(process.env.port||3000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log('listening to 3000');
 });
