de3edb285669b64d0371f74d80d46028bdd9148e
three-cubed <72698356+three-cubed@users.noreply.github.com>
2022-10-18 14:56:39 +0100

    port -> PORT

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.set('view engine', 'ejs');
 const router = require('./routes/routes');
 app.use('/', router);
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening to 3000');
 });
